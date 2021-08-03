83b6c4e78b8141185c3fca3dc10d8aa929f530e9
Gitea <vivek.buzruk@sarvatech.co.in>
2021-08-03 22:39:52 +0530

    Spring Initializer release

---
+++ src/main/java/coin/sarvatech/osSpringDemo/OsSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package coin.sarvatech.osSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coin/sarvatech/osSpringDemo/OsSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package coin.sarvatech.osSpringDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
